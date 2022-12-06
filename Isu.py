d4507a57083e32b210a917c8f3f09ff17ae98780
Isugithub <isulakshani57@gmail.com>
2022-12-06 07:10:08 +0530

    Initial commit

---
+++ Isu.py
@@ -0,0 +1,13 @@
+name = "Isuri Lakshani"
+#name = "123"
+#name = "IsuriLakshani"
+#print(len(name))
+#print(name.find("u"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("s"))
+#print(name.replace("i","u"))
+print(name*4)
---
+++ Nilu.py
@@ -0,0 +1,19 @@
+#name = "Isu"
+#age = 23
+#attractive = False
+
+name,age,attractive = "Isu",23,False
+print(name)
+print(age)
+print(attractive)
+
+#Spongebob = 30
+#Patrick = 30
+#Sandy = 30
+#Cake = 30
+
+Spongebob = Patrick = Sandy = Cake = 30
+print(Spongebob)
+print(Sandy)
+print(Patrick)
+print(Cake)
